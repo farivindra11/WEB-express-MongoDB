0cdc810c332faeac2f0dcf8eff6e12830aff9895
fariv indra <farivind@gmail.com>
2020-08-15 10:57:08 +0700

    in progress

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-var indexRouter = require('./routes/index');
+
 var usersRouter = require('./routes/users');
-
 var app = express();
-
-//Mongo connection
 
 
 // view engine setup
@@ -23,25 +22,46 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Connection URL
+const url = 'mongodb://localhost:27017';
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function (err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  // Database Name
+  const dbName = 'data';
+  // coll for 'data' collection
+  const coll = 'bread'; 
+  const db = client.db(dbName);
+  var indexRouter = require('./routes/index')(db,coll);
+  app.use('/', indexRouter);
+
+
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+  // client.close();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,17 @@ var express = require('express');
 var router = express.Router();
 var moment = require('moment');
 
+var objectid = require('objectid')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// add router
+router.get('/add', (req, res) => {res.status(200).render('add') });
+
+router.get('/edit/:id', (req, res) =>{
+  res.status(200).render('edit')
+});
 module.exports = router;
